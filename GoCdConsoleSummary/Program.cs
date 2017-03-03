c31464ff6d0576c0ad2011ff9ca24bcf4a261052
mat roberts <mat.roberts@biomni.com>
2017-03-03 16:48:18 +0000

    added usage info

--- GoCdConsoleSummary/Program.cs
+++ GoCdConsoleSummary/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GoCdConsoleSummary
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                var info = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+
+                Console.WriteLine($@"{info.InternalName}
+Version {info.ProductVersion}
+{info.LegalCopyright}
+
+{info.Comments}
+
+Usage:
+    {info.InternalName} [output-file]
+Examples:
+    {info.InternalName} Console-Summary.html");
+                Environment.Exit(1);
+            }
         }
     }
 }

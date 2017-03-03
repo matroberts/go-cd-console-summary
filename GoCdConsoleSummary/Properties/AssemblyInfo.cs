a508e43f6ab0941a4feb4491616591a03c8585c2
mat roberts <mat.roberts@biomni.com>
2017-03-03 16:33:24 +0000

    created a new project, and setup the assembly info

---
+++ GoCdConsoleSummary/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("GoCdConsoleSummary")]
+[assembly: AssemblyDescription("Makes an Html page which scans a go.cd console.log for errors and warnings.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoCdConsoleSummary")]
+[assembly: AssemblyCopyright("Copyright © Mat Roberts and James Sinclair 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.*")]
